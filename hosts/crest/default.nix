cd6b4076b5f1fe4e6bbc4920d174755234beac0a
cnqso <wmkelly@umich.edu>
2025-12-18 21:29:56 +0000

    Remove niri config

--- hosts/crest/default.nix
+++ hosts/crest/default.nix
@@ -1,17 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware.nix
-    ../../modules/common.nix
-    ../../modules/desktop.nix
-    ../../modules/dev.nix
-    ../../modules/audio.nix
-    ../../modules/bluetooth.nix
-    ../../modules/home.nix
-  ];
+  imports = [ ./hardware.nix ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "crest";
+  networking.networkmanager.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
@@ -22,11 +18,44 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  programs.niri.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
   environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
     LIBVA_DRIVER_NAME = "nvidia";
     GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
+
+  users.users.cnqso = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    initialPassword = "nixos"; # CHANGE THIS IMMEDIATELY
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    kitty
+    firefox
+    xwayland-satellite
+    fuzzel
+    alacritty
+    neovim
+    wlr-randr
+
+    code-cursor
+    wl-clipboard
+  ];
 
   system.stateVersion = "24.11";
 }
