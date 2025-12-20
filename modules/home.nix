cdf14ade5896f1e0fa8f4d3489a533e2b1d70824
cnqso <wmkelly@umich.edu>
2025-12-20 15:54:43 +0000

    Niri keybinds

--- modules/home.nix
+++ modules/home.nix
@@ -113,8 +113,86 @@
             };
           };
         };
-        # Set DP-1 as preferred output (where niri will spawn windows by default)
+        
         prefer-no-csd = true;
+        
+        # Default keybinds
+        binds = {
+          # Launchers
+          "Mod+T".action.spawn = ["kitty"];
+          "Mod+D".action.spawn = ["fuzzel"];
+          "Mod+Return".action.spawn = ["kitty"];
+          
+          # Window management
+          "Mod+Q".action.close-window = {};
+          "Mod+H".action.focus-column-left = {};
+          "Mod+L".action.focus-column-right = {};
+          "Mod+J".action.focus-window-down = {};
+          "Mod+K".action.focus-window-up = {};
+          "Mod+Left".action.focus-column-left = {};
+          "Mod+Right".action.focus-column-right = {};
+          "Mod+Down".action.focus-window-down = {};
+          "Mod+Up".action.focus-window-up = {};
+          
+          # Moving windows
+          "Mod+Shift+H".action.move-column-left = {};
+          "Mod+Shift+L".action.move-column-right = {};
+          "Mod+Shift+J".action.move-window-down = {};
+          "Mod+Shift+K".action.move-window-up = {};
+          "Mod+Shift+Left".action.move-column-left = {};
+          "Mod+Shift+Right".action.move-column-right = {};
+          "Mod+Shift+Down".action.move-window-down = {};
+          "Mod+Shift+Up".action.move-window-up = {};
+          
+          # Workspaces
+          "Mod+1".action.focus-workspace = 1;
+          "Mod+2".action.focus-workspace = 2;
+          "Mod+3".action.focus-workspace = 3;
+          "Mod+4".action.focus-workspace = 4;
+          "Mod+5".action.focus-workspace = 5;
+          "Mod+6".action.focus-workspace = 6;
+          "Mod+7".action.focus-workspace = 7;
+          "Mod+8".action.focus-workspace = 8;
+          "Mod+9".action.focus-workspace = 9;
+          
+          # Move to workspace
+          "Mod+Shift+1".action.move-window-to-workspace = 1;
+          "Mod+Shift+2".action.move-window-to-workspace = 2;
+          "Mod+Shift+3".action.move-window-to-workspace = 3;
+          "Mod+Shift+4".action.move-window-to-workspace = 4;
+          "Mod+Shift+5".action.move-window-to-workspace = 5;
+          "Mod+Shift+6".action.move-window-to-workspace = 6;
+          "Mod+Shift+7".action.move-window-to-workspace = 7;
+          "Mod+Shift+8".action.move-window-to-workspace = 8;
+          "Mod+Shift+9".action.move-window-to-workspace = 9;
+          
+          # Window sizing
+          "Mod+R".action.switch-preset-column-width = {};
+          "Mod+F".action.maximize-column = {};
+          "Mod+Shift+F".action.fullscreen-window = {};
+          "Mod+C".action.center-column = {};
+          
+          # Monitor navigation
+          "Mod+Shift+Ctrl+Left".action.move-column-to-output-left = {};
+          "Mod+Shift+Ctrl+Right".action.move-column-to-output-right = {};
+          "Mod+Shift+Ctrl+H".action.move-column-to-output-left = {};
+          "Mod+Shift+Ctrl+L".action.move-column-to-output-right = {};
+          
+          # Screenshots
+          "Print".action.screenshot = {};
+          "Ctrl+Print".action.screenshot-screen = {};
+          "Alt+Print".action.screenshot-window = {};
+          
+          # System
+          "Mod+Shift+E".action.quit = {};
+          "Mod+Shift+P".action.power-off-monitors = {};
+          
+          # Media keys
+          "XF86AudioRaiseVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"];
+          "XF86AudioLowerVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
+          "XF86AudioMute".action.spawn = ["wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle"];
+          "XF86AudioMicMute".action.spawn = ["wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle"];
+        };
       };
 
       # Enable stylix for niri theming
